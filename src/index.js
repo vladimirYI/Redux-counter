cc756b87b9650cd7f0e3aad541c0a5ca286daeb6
Vladimir <torna7@yandex.ru>
2020-12-23 17:44:37 +0300

    redux counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state +1;
+        case 'DEC':
+            return state -1;
+        case 'ZERO':
+            return state = action.value;
+        default: 
+            return state;
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const zero = (value) => ({type: 'ZERO', value});
+    
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch(inc());
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch(dec());
+});
+
+document.getElementById('zero').addEventListener('click', () => {
+    const value = 0;
+    store.dispatch(zero(value));
+});
+
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
